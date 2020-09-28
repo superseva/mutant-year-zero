7a035f0673fc98907719513ddb661329474dde43
superseva <superseva@gmail.com>
2020-09-28 13:47:23 +0200

    Added Roll and Dialog adn Chat message. TODO PUSH

---
+++ module/component/dice-roller.js
@@ -0,0 +1,172 @@
+/** This class is responsible for rolling dice */
+export class DiceRoller {
+    dices = [];
+    lastType = "";
+    lastRollName = "";
+    lastDamage = 0;
+
+    /**
+     * @param  {string} rollName   Display name for the roll
+     * @param  {number} base       Number of Base dice
+     * @param  {number} skill      Number of Skill dice
+     * @param  {number} gear       Number of Gear dice
+     * @param  {Array}  artifacts  Array of artifact dice objects: [{dice: number of dice, face: number of faces}]
+     * @param  {number} modifier   Increase/decrease amount of skill dice
+     * @param  {number} [damage=0] Weapon damage
+     */
+    roll({ rollName="Roll Name", base =0, skill =0, gear= 0, artifacts= null, modifier= 0, damage= null } = {}) {
+        this.dices = [];
+        this.lastType = "skill";
+        this.lastRollName = rollName;
+        let computedSkill = skill + modifier;
+        let computedSkillType;
+        if (computedSkill > 0) {
+            computedSkillType = "skill";
+        } else {
+            computedSkill = -computedSkill;
+            computedSkillType = "skill-penalty";
+        }
+        this.rollDice(base, "base", 6, 0);
+        this.rollDice(computedSkill, computedSkillType, 6, 0);
+        this.rollDice(gear, "gear", 6, 0);
+        if (artifacts) {
+            artifacts.forEach(artifact => {
+                this.rollDice(artifact.dice, "artifact", artifact.face);
+            });
+        }
+        let computedDamage = damage;
+        if (damage) {
+            if (damage > 0) {
+                computedDamage = computedDamage - 1;
+            }
+            this.lastDamage = computedDamage;
+        }
+        this.sendRollToChat(false);
+    }
+
+    /**
+     * Roll a set of dice
+     * 
+     * @param  {number} numberOfDice     How many dice to roll
+     * @param  {string} typeOfDice       Base/skill/gear
+     * @param  {number} numberOfFaces    What dice to roll
+     * @param  {number} automaticSuccess For mutations
+     */
+    rollDice(numberOfDice, typeOfDice, numberOfFaces, automaticSuccess) {
+        if (numberOfDice > 0) {
+            let die = new Die(numberOfFaces);
+            die.roll(numberOfDice);
+            console.log(die.results);
+            die.results.forEach((result) => {
+                if (automaticSuccess > 0) {
+                    result = numberOfFaces;
+                    automaticSuccess -= 1;
+                }
+                let successAndWeight = this.getSuccessAndWeight(result, typeOfDice);
+                this.dices.push({
+                    value: result,
+                    type: typeOfDice,
+                    success: successAndWeight.success,
+                    weight: successAndWeight.weight,
+                    face: numberOfFaces,
+                });
+            });
+        }
+    }
+
+    /**
+     * Retrieves amount of successes from a single die
+     * and weight for ordering during display
+     * 
+     * @param  {number} diceValue
+     * @param  {string} diceType
+     */
+    getSuccessAndWeight(diceValue, diceType) {
+        if (diceValue === 6) {
+            if (diceType === "skill-penalty") {
+                return { success: -1, weight: -1 };
+            } else {
+                return { success: 1, weight: 1 };
+            }
+        }      
+        else if (diceValue === 1 && diceType !== "skill-penalty" && diceType !== "skill") {
+            return { success: 0, weight: -2 };
+        } else {
+            return { success: 0, weight: 0 };
+        }
+    }
+
+    /**
+     * Display roll in chat
+     * 
+     * @param  {boolean} isPushed Whether roll was pushed
+     */
+    async sendRollToChat(isPushed) {
+        this.dices.sort(function (a, b) {
+            return b.weight - a.weight;
+        });
+        let numberOfSuccesses = this.countSuccesses();
+        let numberOfFailures = this.countFailures();
+        let numberOfGearFailures = this.countGearFailures();
+        let rollData = {
+            name: this.lastRollName,
+            isPushed: isPushed,
+            isSpell: false,
+            successes: numberOfSuccesses,
+            failures: numberOfFailures,
+            gearfailures: numberOfGearFailures,
+            damage: numberOfSuccesses + this.lastDamage,
+            dices: this.dices
+        };
+        const html = await renderTemplate("systems/mutant-year-zero/templates/chat/roll.html", rollData);
+        let chatData = {
+            user: game.user._id,
+            rollMode: game.settings.get("core", "rollMode"),
+            content: html,
+        };
+        if (["gmroll", "blindroll"].includes(chatData.rollMode)) {
+            chatData.whisper = ChatMessage.getWhisperIDs("GM");
+        } else if (chatData.rollMode === "selfroll") {
+            chatData.whisper = [game.user];
+        }
+        await ChatMessage.create(chatData);
+    }
+
+    /**
+     * Count total successes
+     */
+    countSuccesses() {
+        let result = 0;
+        this.dices.forEach(dice => {
+            result = result + dice.success;
+        });
+        return result;
+    }
+
+    /**
+     * Count total failures
+     */
+    countFailures() {
+        let result = 0;
+        this.dices.forEach(dice => {
+            if (dice.value === 1 && dice.type === "base") {
+                result++;
+            }
+        });
+        return result;
+    }
+
+    /**
+     * Count gear failures
+     */
+    countGearFailures() {
+        let result = 0;
+        this.dices.forEach(dice => {
+            if (dice.value === 1 && dice.type === "gear") {
+                result++;
+            }
+        });
+        return result;
+    }
+
+}
--- module/myz.js
+++ module/myz.js
@@ -4,52 +4,59 @@ import { BoilerplateActorSheet } from "./actor/actor-sheet.js";
 import { BoilerplateItem } from "./item/item.js";
 import { BoilerplateItemSheet } from "./item/item-sheet.js";
 
-Hooks.once('init', async function() {
+import { DiceRoller } from "./component/dice-roller.js";
+import { RollDialog } from "./app/roll-dialog.js";
 
-  game.boilerplate = {
-    BoilerplateActor,
-    BoilerplateItem,
-    rollItemMacro
-  };
+Hooks.once('init', async function () {
 
-  /**
-   * Set an initiative formula for the system
-   * @type {String}
-   */
-  CONFIG.Combat.initiative = {
-    formula: "1d20 + @abilities.dex.mod",
-    decimals: 2
-  };
+    game.myz = {
+        BoilerplateActor,
+        BoilerplateItem,
+        rollItemMacro,
+        DiceRoller,
+        RollDialog
+    };
 
-  // Define custom Entity classes
-  CONFIG.Actor.entityClass = BoilerplateActor;
-  CONFIG.Item.entityClass = BoilerplateItem;
+    /**
+     * Set an initiative formula for the system
+     * @type {String}
+     */
+    CONFIG.Combat.initiative = {
+        formula: "1d20 + @abilities.dex.mod",
+        decimals: 2
+    };
 
-  // Register sheet application classes
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("boilerplate", BoilerplateActorSheet, { makeDefault: true });
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("boilerplate", BoilerplateItemSheet, { makeDefault: true });
+    // Define custom Entity classes
+    CONFIG.Actor.entityClass = BoilerplateActor;
+    CONFIG.Item.entityClass = BoilerplateItem;
 
-  // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
+    //CONFIG.diceRoller = new DiceRoller();
 
-  Handlebars.registerHelper('toLowerCase', function(str) {
-    return str.toLowerCase();
-  });
+    // Register sheet application classes
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("boilerplate", BoilerplateActorSheet, { makeDefault: true });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("boilerplate", BoilerplateItemSheet, { makeDefault: true });
+
+    // If you need to add Handlebars helpers, here are a few useful examples:
+    Handlebars.registerHelper('concat', function () {
+        var outStr = '';
+        for (var arg in arguments) {
+            if (typeof arguments[arg] != 'object') {
+                outStr += arguments[arg];
+            }
+        }
+        return outStr;
+    });
+
+    Handlebars.registerHelper('toLowerCase', function (str) {
+        return str.toLowerCase();
+    });
 });
 
-Hooks.once("ready", async function() {
-  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on("hotbarDrop", (bar, data, slot) => createBoilerplateMacro(data, slot));
+Hooks.once("ready", async function () {
+    // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+    Hooks.on("hotbarDrop", (bar, data, slot) => createBoilerplateMacro(data, slot));
 });
 
 /* -------------------------------------------- */
@@ -64,24 +71,24 @@ Hooks.once("ready", async function() {
  * @returns {Promise}
  */
 async function createBoilerplateMacro(data, slot) {
-  if (data.type !== "Item") return;
-  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
-  const item = data.data;
+    if (data.type !== "Item") return;
+    if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+    const item = data.data;
 
-  // Create the macro command
-  const command = `game.boilerplate.rollItemMacro("${item.name}");`;
-  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
-  if (!macro) {
-    macro = await Macro.create({
-      name: item.name,
-      type: "script",
-      img: item.img,
-      command: command,
-      flags: { "boilerplate.itemMacro": true }
-    });
-  }
-  game.user.assignHotbarMacro(macro, slot);
-  return false;
+    // Create the macro command
+    const command = `game.boilerplate.rollItemMacro("${item.name}");`;
+    let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+    if (!macro) {
+        macro = await Macro.create({
+            name: item.name,
+            type: "script",
+            img: item.img,
+            command: command,
+            flags: { "boilerplate.itemMacro": true }
+        });
+    }
+    game.user.assignHotbarMacro(macro, slot);
+    return false;
 }
 
 /**
@@ -91,13 +98,13 @@ async function createBoilerplateMacro(data, slot) {
  * @return {Promise}
  */
 function rollItemMacro(itemName) {
-  const speaker = ChatMessage.getSpeaker();
-  let actor;
-  if (speaker.token) actor = game.actors.tokens[speaker.token];
-  if (!actor) actor = game.actors.get(speaker.actor);
-  const item = actor ? actor.items.find(i => i.name === itemName) : null;
-  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+    const speaker = ChatMessage.getSpeaker();
+    let actor;
+    if (speaker.token) actor = game.actors.tokens[speaker.token];
+    if (!actor) actor = game.actors.get(speaker.actor);
+    const item = actor ? actor.items.find(i => i.name === itemName) : null;
+    if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
 
-  // Trigger the item roll
-  return item.roll();
+    // Trigger the item roll
+    return item.roll();
 }
